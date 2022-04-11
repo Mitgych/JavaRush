e56e3b8c505d3c8113eb4e8e970b5bca90c6ae4d
Vlad Mitrinyuk <vladmitrinyuk@gmail.com>
2022-04-11 15:48:21 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,18 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class Main {
+class FixingStringIndexOutOfBoundsException {
 
-    public static void inverseFlags(boolean[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = !arr[i];
-        }
-    }
-
-    /* Do not change code below */
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        final List<Boolean> booleans = Arrays
-                .stream(scanner.nextLine().split("\\s+"))
-                .map(Boolean::parseBoolean)
-                .collect(Collectors.toList());
-        final boolean[] flags = new boolean[booleans.size()];
-        for (int i = 0; i < flags.length; i++) {
-            flags[i] = booleans.get(i);
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+        int index = scanner.nextInt();
+
+        try {
+            System.out.println(string.charAt(index));
+        } catch (Exception e) {
+            System.out.println("Out of bounds!");
         }
-        inverseFlags(flags);
-        final String representation = Arrays.toString(flags)
-                .replace("[", "")
-                .replace("]", "")
-                .replace(",", "");
-        System.out.println(representation);
+        
     }
 }

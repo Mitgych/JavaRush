d5aaa0cddca9f0d166d3c17e2e7c8936a11f1edd
Vlad Mitrinyuk <vladmitrinyuk@gmail.com>
2022-04-12 07:29:23 +0300

    Добавил обработку ошибок

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -12,25 +12,30 @@ public class Cinema {
     public static String[][] cinemaHall;
     //Statistics variables
     public static int countSoldTicket = 0;
-    public static double countSoldTicketPersent = 0;
+    public static double countSoldTicketPercent = 0.00;
     public static int currIncome = 0;
     public static int totalIncome;
 
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        //Read sizes of cinema
-        System.out.println("Enter the number of rows:");
-        y = scanner.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        x = scanner.nextInt();
+        while (true) {
+            try (Scanner scanner = new Scanner(System.in)){
+                //Read sizes of cinema
+                System.out.println("Enter the number of rows:");
+                y = scanner.nextInt();
+                System.out.println("Enter the number of seats in each row:");
+                x = scanner.nextInt();
 
-        cinemaHall = new String[y][x]; //initialize cinemaHall
-        for (String[] i: cinemaHall) { //set start parameters in cinemaHall
-            for (int j = 0; j < x; j++) {
-                i[j] = "S";
+                cinemaHall = new String[y][x]; //initialize cinemaHall
+                for (String[] i : cinemaHall) { //set start parameters in cinemaHall
+                    for (int j = 0; j < x; j++) {
+                        i[j] = "S";
+                    }
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Wrong input!");
             }
         }
 
@@ -41,44 +46,58 @@ public class Cinema {
         }
 
         //start menu
-        int choice = 1;
-        while (choice != 0) {
-            menu();
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1 -> printCinemaHall();
-                case 2 -> buy();
-                case 3 -> statistics();
-
+        try (Scanner scanner = new Scanner(System.in)){
+            int choice = 1;
+            while (choice != 0) {
+                menu();
+                choice = scanner.nextInt(); // TODO: ѕроблема со сканером
+                if (choice >= 0 && choice <= 3) {
+                    switch (choice) {
+                        case 1 -> printCinemaHall();
+                        case 2 -> buy();
+                        case 3 -> statistics();
+                    }
+                } else {
+                    System.out.println("Wrong input!");
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        scanner.close();
     }
 
 
     public static void buy() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nEnter a row number:");
-        ySeat = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        xSeat = scanner.nextInt();
-        cinemaHall[ySeat - 1][xSeat - 1] = "B";
+        while (true) {
+            try (Scanner scanner = new Scanner(System.in);){
+                System.out.println("Enter a row number:");
+                ySeat = scanner.nextInt();
+                System.out.println("Enter a seat number in that row:");
+                xSeat = scanner.nextInt();
+                if ("B".equals(cinemaHall[ySeat - 1][xSeat - 1])) {
+                    System.out.println("That ticket has already been purchased!");
+                    continue;
+                }
+                cinemaHall[ySeat - 1][xSeat - 1] = "B";
+                break;
+            } catch (Exception e) {
+                System.out.println("Wrong input!");
+            }
 
-        //write ticket price
-        if (x * y <= 60) {
-            price = 10;
-        } else if (ySeat <= y / 2) {
-            price = 10;
-        } else {
-            price = 8;
+            //write ticket price
+            if (x * y <= 60) {
+                price = 10;
+            } else if (ySeat <= y / 2) {
+                price = 10;
+            } else {
+                price = 8;
+            }
+            System.out.printf("\nTicket price: $%d", price);
+            System.out.println();
+            countSoldTicket += 1;
+            countSoldTicketPercent = (double) countSoldTicket / x / y * 100;
+            currIncome += price;
         }
-        System.out.printf("\nTicket price: $%d", price);
-        System.out.println();
-
-        countSoldTicket += 1;
-        // TODO: разобратьс€ с рассчетом в double
-        countSoldTicketPersent = countSoldTicket / x * y;
-        currIncome += price;
     }
 
 
@@ -97,16 +116,19 @@ public class Cinema {
             }
             System.out.println();
         }
-        System.out.println();
     }
 
     //print menu
     public static void menu() {
-        System.out.println("1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit\n");
+        System.out.println("\n1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit\n");
     }
 
     // TODO: finish this method
     public static void statistics() {
+        System.out.printf("\nNumber of purchased tickets: %d\n", countSoldTicket);
+        System.out.printf("Percentage: %.2f%%\n", countSoldTicketPercent);
+        System.out.printf("Current income: $%d\n", currIncome);
+        System.out.printf("Total income: $%d\n", totalIncome);
 
     }
 }

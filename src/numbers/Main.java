11daa6704699863a889759b7415d1d48dcae0a84
Vlad Mitrinyuk <vladmitrinyuk@gmail.com>
2022-04-13 17:29:57 +0300

    + to last commit

--- src/numbers/Main.java
+++ src/numbers/Main.java
@@ -32,63 +32,11 @@ public class Main {
             //    }
             //}
             Number number = new Number(readNumber);
-            System.out.println("Properties of " + number.value +
-                    "\neven: " +  number.isEven + 
-                    "\nodd: " + number.isOdd + 
-                    "\nbuzz: " + number.isBuzz + 
-                    "\nduck: " + number.isDuck);
+            number.print();
         } else {
             System.out.println("This number is not natural!");
         }
     }
 
-    public static class Number {
-        private int value;
-        private boolean isBuzz;
-        //private boolean divisibleBySeven;
-        //private boolean EndsWithSeven;
-        private boolean isOdd;
-        private boolean isEven;
-        private boolean isDuck;
 
-        public Number(int number) {
-            this.value = number;
-            
-            if (number % 2 == 0) {
-                this.isOdd = false;
-                this.isEven = true;
-            } else {
-                this.isOdd = true;
-                this.isEven = false;
-            }
-
-            //if (number % 10 == 7) {
-            //    this.EndsWithSeven= true;
-            //} else {
-            //    this.EndsWithSeven= false;
-            //}
-//
-            //if (number % 7 == 0) {
-            //    this.divisibleBySeven= true;
-            //} else {
-            //    this.divisibleBySeven= false;
-            //}
-
-            if (number % 7 == 0 || number % 10 == 7) {
-                this.isBuzz = true;
-            } else {
-                this.isBuzz = false;
-            }
-
-            String numberAsString = Integer.toString(number);
-            if (numberAsString.charAt(0) == '0') {
-                numberAsString = numberAsString.substring(1);
-            }
-            if (numberAsString.contains("0")) {
-                this.isDuck = true;
-            } else {
-                this.isDuck = false;
-            }
-        }
-    }
 }

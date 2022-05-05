ee5dfcc8b33e458879a2950a4c1d8a32c813dcc8
Vlad Mitrinyuk <vladmitrinyuk@gmail.com>
2022-05-05 20:27:50 +0300

    passed 6 stage

--- src/numbers/Main.java
+++ src/numbers/Main.java
@@ -7,15 +7,16 @@ public class Main {
             "- enter a natural number to know its properties;\n" +
             "- enter two natural numbers to obtain the properties of the list:\n" +
             "  * the first parameter represents a starting number;\n" +
-            "  * the second parameter shows how many consecutive numbers are to be processed;\n" +
-            "- two natural numbers and a property to search for;\n" +
+            "  * the second parameters show how many consecutive numbers are to be processed;\n" +
+            "- two natural numbers and two properties to search for;\n" +
             "- separate the parameters with one space;\n" +
-            "- enter 0 to exit.\n";
+            "- enter 0 to exit.";
 
     public static void main(String[] args) {
         long firstNumber;
         long secondNumber;
         String property;
+        String property2;
         List<String> buffer;
 
         System.out.println("Welcome to Amazing Numbers!\n");
@@ -29,10 +30,27 @@ public class Main {
                 break;
             }
 
-            if (buffer.size() == 3) { //input two numbers and property
+            if (buffer.size() == 4) { //input two numbers and two property
                 firstNumber = Long.parseLong(buffer.get(0));
                 secondNumber = Long.parseLong(buffer.get(1));
-                property = buffer.get(2).toUpperCase();
+                property = buffer.get(2);
+                property2 = buffer.get(3);
+
+                int count = 0;
+                while (count < secondNumber) {
+                    Number num = new Number(firstNumber);
+                    if (num.isProperty(property) && num.isProperty(property2)) {
+                        num.printList();
+                        count++;
+                    }
+                    firstNumber++;
+                }
+                System.out.println();
+            } else if (buffer.size() == 3) { //input two numbers and one property
+                firstNumber = Long.parseLong(buffer.get(0));
+                secondNumber = Long.parseLong(buffer.get(1));
+                property = buffer.get(2);
+
                 int count = 0;
                 while (count < secondNumber) {
                     Number num = new Number(firstNumber);
@@ -42,15 +60,18 @@ public class Main {
                     }
                     firstNumber++;
                 }
+                System.out.println();
             } else if (buffer.size() == 2) { // input two numbers
                 firstNumber = Long.parseLong(buffer.get(0));
                 secondNumber = Long.parseLong(buffer.get(1));
+
                 for (int i = 0; i < secondNumber; i++) {
                     Number num = new Number(firstNumber + i);
                     num.printList();
                 }
             } else if (buffer.size() == 1) { //input one number
                 firstNumber = Long.parseLong(buffer.get(0));
+
                 Number numb = new Number(firstNumber);
                 numb.print();
             }
--- src/numbers/ReadInput.java
+++ src/numbers/ReadInput.java
@@ -11,36 +11,60 @@ public class ReadInput {
     static final String ERROR2 = "The second parameter should be a natural number.\n";
     static final String ERROR3 = "The property [%s] is wrong.\n" +
             "Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SUNNY, SQUARE]";
-    static final String ERROR4 = "The property [%s, %s] are wrong.\n" +
+    static final String ERROR4 = "The properties [%s, %s] are wrong.\n" +
             "Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SUNNY, SQUARE]";
-    static final String[] properties = {"EVEN", "ODD", "BUZZ", "DUCK", "PALINDROMIC",
+    static final String ERROR5 = "The request contains mutually exclusive properties: [%s, %s]\n" +
+            "There are no numbers with these properties.";
+    static final String[] PROPERTIES = {"EVEN", "ODD", "BUZZ", "DUCK", "PALINDROMIC",
             "GAPFUL", "SPY", "SUNNY", "SQUARE"};
+    static final String[][] EXCLUSIVE_PROPERTIES = {
+            {"EVEN", "ODD"}, {"ODD", "EVEN"},
+            {"DUCK", "SPY"}, {"SPY", "DUCK"},
+            {"SUNNY", "SQUARE"}, {"SQUARE", "SUNNY"}
+    };
 
     public static List<String> input() {
         Scanner scanner  = new Scanner(System.in);
         List<String> result = new ArrayList<>(Arrays
-                .stream(scanner.nextLine().split("\\s+"))
+                .stream(scanner.nextLine().toUpperCase().split("\\s+"))
                 .collect(Collectors.toList()));
 
         if (result.isEmpty()) { //check empty input
             System.out.println(Main.INSTRUCTION);
             result = input();
-        } else if (result.size() > 0 && Long.parseLong(result.get(0)) < 0) { //check first number < 0
+        } else if (Long.parseLong(result.get(0)) < 0) { //check first number < 0
             System.out.println(ERROR1);
             System.out.print("\nEnter a request: ");
             result = input();
-        } else if (result.size() > 1 && Long.parseLong(result.get(1)) < 0) {
+        } else if (result.size() > 1 && Long.parseLong(result.get(1)) <= 0) { //check second number < 0
             System.out.println(ERROR2);
             System.out.print("\nEnter a request: ");
             result = input();
-        } else if (result.size() > 2 && Arrays.stream(properties).noneMatch(result.get(2).toUpperCase()::equals)) {
-            System.out.printf(ERROR3, result.get(2));
-            System.out.print("\nEnter a request: ");
-            result = input();
-        } else if (result.size() > 3 && Arrays.stream(properties).noneMatch(result.get(3).toUpperCase()::equals)) {
-            System.out.printf(ERROR4, result.get(2), result.get(3));
-            System.out.print("\nEnter a request: ");
-            result = input();
+        } else if (result.size() > 2 ) { //check properties
+            List<String> buff = new ArrayList<>();
+            final String[] properties2 = result.subList(2, result.size()).toArray(new String[2]);
+
+            //write in buff wrong properties
+            for (String str: result.subList(2, result.size())) {
+                if (Arrays.stream(PROPERTIES).noneMatch(str::equals)){
+                    buff.add(str);
+                }
+            }
+
+            //print error depending on number of wrong properties in buff. and check exclusive properties
+            if (buff.size() == 1) {
+                System.out.printf(ERROR3, buff.get(0));
+                System.out.print("\nEnter a request: ");
+                result = input();
+            } else if (buff.size() == 2){
+                System.out.printf(ERROR4, buff.get(0), buff.get(1));
+                System.out.print("\nEnter a request: ");
+                result = input();
+            } else if (Arrays.stream(EXCLUSIVE_PROPERTIES).anyMatch(e -> Arrays.equals(e, properties2))) {
+                System.out.printf(ERROR5, result.get(2), result.get(3));
+                System.out.print("\nEnter a request: ");
+                result = input();
+            }
         }
         return result;
     }
